97b9e1132f87a0db161ffe73a573f5a0d234592d
Pamella Bezerra <pbsf2@cin.ufpe.br>
2018-08-03 17:10:51 -0300

    Add logging info

--- tuirer/settings/production.py
+++ tuirer/settings/production.py
@@ -3,3 +3,19 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
